e4c943f2cc0ed5518ee092515ae4d3f8232ac27f
mateorussi <73946312+mateorussi@users.noreply.github.com>
2021-03-16 11:38:28 +0100

    15-03-21

--- practica_4_3/src/p4_3/Ejercicio_6.java
+++ practica_4_3/src/p4_3/Ejercicio_6.java
@@ -18,86 +18,87 @@ public class Ejercicio_6 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		String tablaNotas[][] = new String[4][16];
-
-//		int numAlumnos = Integer.parseInt(entrada.nextLine());
 		
-		double[] notasN = new double[tablaNotas];
-		String[] notasS = new String[tablaNotas];
-
-		float nMediaAlumno, nMediaAsignatura, nMediaClase;
-//		Datos asignaturas en las filas
-		tablaNotas[0][0] = "Asignatura";
-		tablaNotas[1][0] = "PROG";
-		tablaNotas[2][0] = "FOL";
-		tablaNotas[3][0] = "BBDD";
-
-//		 Datos alumnos en columnas 
-		tablaNotas[0][1] = "Alumno 1";
-		tablaNotas[0][2] = "Alumno 2";
-		tablaNotas[0][3] = "Alumno 3";
-		tablaNotas[0][4] = "Alumno 4";
-		tablaNotas[0][5] = "Alumno 5";
-		tablaNotas[0][6] = "Alumno 6";
-		tablaNotas[0][7] = "Alumno 7";
-		tablaNotas[0][8] = "Alumno 8";
-		tablaNotas[0][9] = "Alumno 9";
-		tablaNotas[0][10] = "Alumno 10";
-		tablaNotas[0][11] = "Alumno 11";
-		tablaNotas[0][12] = "Alumno 12";
-		tablaNotas[0][13] = "Alumno 13";
-		tablaNotas[0][14] = "Alumno 14";
-		tablaNotas[0][15] = "Alumno 15";
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		
+		int tablaNotas[][] = new int[3][15];
+		
+		float mN, mA;
+		
+		System.out.println("Generacion de las notas");
 		
 		for (int x = 0; x < tablaNotas.length; x++) {
-			for (int y = 0; y < tablaNotas.length; y++) {
-				do {
-					
-					tablaNotas[x][y]=String.valueOf(Math.random()*14+1);
-			
-					if (notasN[i] >= 0 && notasN[i] < 5) {
-						notasS[i] = "Suspenso";
-					} else if (notasN[i] >= 5 && notasN[i] < 7) {
-						notasS[i] = "Bien";
-					} else if (notasN[i] >= 7 && notasN[i] < 9) {
-						notasS[i] = "Notable";
-					} else if (notasN[i] >= 9 && notasN[i] <= 10) {
-						notasS[i] = "Sobresaliente";
-					} else {
-						notasS[i] = "Nota erronea";
-					}
-				} while (notasN[i] < 0 || notasN[i] > 10);
+			for (int y = 0; y < tablaNotas[0].length; y++) {
+				tablaNotas[x][y]=(int) (Math.random()*11);
 			}
-			
 		}
-		entrada.close();
-		for (int i = 0; i < nombres.length; i++) {
-			System.out.println("La nota de " + nombres[i] + " es " + notasN[i] + ", " + notasS[i]);
-		}
+		
+		//visualizar notas
+		
+		visualizar(tablaNotas);
+		
+		//media alumnos
+		
+		mN= mediaAlumnos(tablaNotas);
+		
+		//media asignaturas
+		
+		mA= mediaAsignatura(tablaNotas);
+		
+		//visualizar notas medias 
+		
+		System.out.println("La nota media de la clase es: "+mN);
+		
+		System.out.println("La nota media de la clase es: "+mA);
+		
+		
 
-// 
-
-//		
-//		// Asignacion de notas de alumnos
-//		for (int x = 0; x < tablaNotas.length; x++) {
-//			for (int y = 0; y < tablaNotas.length; y++) {
-//				tablaNotas[x][y]= (int) (Math.random()*10+1;
-//			}
-//		}
-//		
-//		// Visualizar Array
-//		
-//		for (int x = 0; x < tablaNotas.length; x++) {
-//			System.out.println("");
-//			for (int y = 0; y < tablaNotas.length; y++) {
-//				System.out.print("\t"+tablaNotas[x][y]);
-//			}
-//
-//		}
 
 	}
+
+		private static float mediaAlumnos(int[][] tablaNotas) {
+			int suma=0;
+			
+			float nota=0, total=0;
+			
+			for (int x = 0; x < tablaNotas.length; x++) {
+				for (int y = 0; y < tablaNotas[x].length; y++) {
+					suma += tablaNotas[x][y];
+					
+					System.out.println("\nLa nota media del alumno "+x+" es: "+(nota=(float)suma/FIL));
+					total += nota;
+					suma=0;
+				}
+			}
+			return total/15;
+		}
+
+		private static float mediaAsignatura(int[][] tablaNotas) {
+			int suma=0;
+			
+			float nota=0, total=90;
+			
+			for (int x = 0; x < tablaNotas.length; x++) {
+				for (int y = 0; y < tablaNotas[x].length; y++) {
+					suma += tablaNotas[x][y];
+				}
+				System.out.println("La nota media de la asignatura "+x+" es: "+(nota=(float)suma/COL));
+				total += nota;
+				suma=0;
+			}
+			
+			return total/3;
+		}
+
+		private static void visualizar(int[][] tablaNotas) {
+			for (int x = 0; x < tablaNotas.length; x++) {
+				System.out.println("");
+				for (int y = 0; y < tablaNotas.length; y++) {
+					System.out.print("\t"+tablaNotas[x][y]);
+				}
+
+			}
+		}
 
 }
